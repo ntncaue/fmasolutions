9209f575896167ace0bbdc9aadb5e07adb42ea4d
marcelooou <marcelosiqueirabonfim@gmail.com>
2024-11-22 22:57:55 -0300

    página acesso energia

---
+++ fma/src/app/solucoes/acesso-energia/page.tsx
@@ -0,0 +1,20 @@
+"use client"
+import React from 'react';
+
+const Acesso: React.FC = () => {
+    return (
+            <main className="w-full">
+
+                {/* Hero Section */}
+                <section className="bg-[#161616] bg-cover bg-center p-5">
+                    <div className="text-white">
+                    <h2 className="xl:text-9xl lg:text-7xl md:text-6xl text-6xl font-bold text-center mb-5 leading-none">
+                            ACESSO UNIVERSAL À ENERGIA
+                        </h2>
+                    </div>
+                </section>
+            </main>
+    );
+};
+
+export default Acesso;
